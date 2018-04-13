ea5e94318023c2bcd4df0ef89337f6628cf0d260
Viktharien <viktharien@zoho.com>
2018-04-13 16:29:46 +0000

    set up controller actions, routes and private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,4 +13,10 @@ class UsersController < ApplicationController
        end
     end
     
+    private
+        
+        def user_params
+           params.require(:user).permit(:first_name, :last_name, :email, :password) 
+        end
+    
 end
