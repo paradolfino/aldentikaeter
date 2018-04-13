14711cc7b6df211ff9542a465207c9c04237310c
Viktharien <viktharien@zoho.com>
2018-04-13 16:27:49 +0000

    set up controller actions, routes and private methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+    def new
+       @user = User.new 
+    end
+    
+    def create
+       @user = User.new(user_params)
+       if @user.save
+           
+       else
+           
+       end
+    end
+    
 end
