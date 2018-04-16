fe23b72b6fe6255cd92c7438bd964a1f9115a23e
Viktharien <viktharien@zoho.com>
2018-04-16 16:39:26 +0000

    create sessions controller, routes, and login form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
     root 'users#index'
     get '/signup' => 'users#new'
     post '/signup' => 'users#create'
