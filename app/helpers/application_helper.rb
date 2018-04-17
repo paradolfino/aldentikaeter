a321e3186de64c5b5d85fe50bdd1d83584fe149a
Viktharien <viktharien@zoho.com>
2018-04-17 16:26:52 +0000

    set up view helper to take care of business logic in view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,10 @@ module ApplicationHelper
     end
     
     def navigation
-        
+        if current_user
+            "<h3>#{link_to 'Log Out', logout_path, method: 'delete'}</h3>".html_safe
+        else
+            "<h3>Not a member? #{link_to 'Sign Up!', signup_path}</h3> or #{login_link_to}"
+        end
     end
 end
